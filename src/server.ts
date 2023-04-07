4c6f8f700677c3356c3686d1e8a14395fc1b9417
João Victor Rocha <joao2013rocha28@hotmail.com>
2023-04-07 18:33:37 -0300

    Iniciando adminjs

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express'
 import { sequelize } from './database'
+import { adminJS, adminJSRouter } from './adminjs'
 
 const app = express()
+
+//app.use(caminho, rotas)
+app.use(express.static('public'))
+app.use(adminJS.options.rootPath, adminJSRouter)
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
