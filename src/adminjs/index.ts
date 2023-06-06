73ea4a508345ba3f6b428247be3427d8a480a679
João Victor Rocha <joao2013rocha28@hotmail.com>
2023-06-05 22:24:22 -0300

    autentificacao e tela de login

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,6 +3,8 @@ import AdminJSExpress from '@adminjs/express'
 import AdminJSSequelize from '@adminjs/sequelize'
 import { sequelize } from '../database'
 import { adminJsResources } from './resources'
+import { User } from '../models'
+import bcrypt from 'bcrypt'
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
@@ -33,4 +35,31 @@ export const adminJS = new AdminJS({
   }
 })
 
-export const adminJSRouter = AdminJSExpress.buildRouter(adminJS)
+export const adminJSRouter = AdminJSExpress.buildAuthenticatedRouter(
+  adminJS,
+  {
+    //usando a autentificaçao para ver se o usuário exite ou se esta correto , para fazer login
+    //os dois metodos abaixo sao obrigatorios, authenticate e cookiespassword
+    authenticate: async (email, password) => {
+      //vai receber o email e a senha do usuário como parametro
+      const user = await User.findOne({ where: { email } }) // vai ser criada a constante buscando o usuário pelo email, também sera verificado se o usuário é admin ou nao
+      if (user && user.role === 'admin') {
+        // se der true vai verificar usando o bcrypt , se a senha digitada é  a mesma do usuário
+        const matched = await bcrypt.compare(password, user.password)
+
+        if (matched) {
+          //se for a mesma vai retornar o usuário e liberar o login
+          return user
+        }
+        //se a senha for diferente ou nao existir o usuário vai voltar false , impedindo o login
+        return false
+      }
+    },
+    cookiePassword: 'senha-do-cookie'
+  },
+  null,
+  {
+    resave: false,
+    saveUninitialized: false
+  }
+)
